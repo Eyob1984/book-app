5299fffa5e7154bdc5f57a76e6e64387f918d4bc
Eyob Tamir <51892714+Eyob1984@users.noreply.github.com>
2023-07-28 08:06:22 -0700

    buttons added on nav bar

---
+++ src/Nav.jsx
@@ -0,0 +1,36 @@
+import React, {useState} from 'react'
+
+
+function Nav() {
+  const [showForm, setShowForm] = useState(false)
+
+  const handleTitleButtonClick = () => {
+    setShowForm(true)
+  }
+
+  const handleAllButtonClick = () => {
+    setShowForm(false)
+  }
+  return (
+    <div>
+      <p>Book App</p>
+      <div>
+        <button onClick={handleAllButtonClick}>See all Books</button>
+        <button onClick={handleTitleButtonClick}>Search by Title</button>
+        {showForm && (
+          <form>
+          <label htmlFor='search'>Search</label>
+           <input
+             type='text'
+             id='search'
+             />  
+             <button type='submit'>Submit</button>     
+       </form>
+        )}
+        
+      </div>
+    </div>
+  )
+}
+
+export default Nav
